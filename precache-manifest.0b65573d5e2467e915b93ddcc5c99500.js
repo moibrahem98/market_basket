00eed6739b41c2492232dbbcfe2326259adab154
moibrahem98 <mo.ibrahiim98@gmail.com>
2022-06-13 16:33:29 +0300

    Updates

---
+++ precache-manifest.0b65573d5e2467e915b93ddcc5c99500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abfc381d3c077ab7fbaafd6580c73cc",
+    "url": "/moibrahem98/grocery-bud/index.html"
+  },
+  {
+    "revision": "80e18929c1b7ec35433c",
+    "url": "/moibrahem98/grocery-bud/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "8dc858ad2f5199b0af5a",
+    "url": "/moibrahem98/grocery-bud/static/js/2.2486421f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moibrahem98/grocery-bud/static/js/2.2486421f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e18929c1b7ec35433c",
+    "url": "/moibrahem98/grocery-bud/static/js/main.87bb0c1e.chunk.js"
+  },
+  {
+    "revision": "a9a0e19f0350ca2921e3",
+    "url": "/moibrahem98/grocery-bud/static/js/runtime-main.86aabf96.js"
+  }
+]);
