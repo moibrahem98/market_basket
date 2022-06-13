45768ec225697905da245369baf43dd4fec6b9bf
moibrahem98 <mo.ibrahiim98@gmail.com>
2022-06-13 16:57:53 +0300

    edit routes !!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Routes, Route } from "react-router-dom";
 import List from './List';
 import Alert from './Alert';
 const getLocalStorage = () => {
@@ -62,33 +63,37 @@ function App() {
     localStorage.setItem('list', JSON.stringify(list));
   }, [list]);
   return (
-    <section className='section-center'>
-      <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+    <Routes>
+      <Route exact path="/market_basket">
+        <section className='section-center'>
+          <form className='grocery-form' onSubmit={handleSubmit}>
+            {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
 
-        <h3>grocery bud</h3>
-        <div className='form-control'>
-          <input
-            type='text'
-            className='grocery'
-            placeholder='e.g. eggs'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button type='submit' className='submit-btn'>
-            {isEditing ? 'edit' : 'submit'}
-          </button>
-        </div>
-      </form>
-      {list.length > 0 && (
-        <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} editItem={editItem} />
-          <button className='clear-btn' onClick={clearList}>
-            clear items
-          </button>
-        </div>
-      )}
-    </section>
+            <h3>grocery bud</h3>
+            <div className='form-control'>
+              <input
+                type='text'
+                className='grocery'
+                placeholder='e.g. eggs'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <button type='submit' className='submit-btn'>
+                {isEditing ? 'edit' : 'submit'}
+              </button>
+            </div>
+          </form>
+          {list.length > 0 && (
+            <div className='grocery-container'>
+              <List items={list} removeItem={removeItem} editItem={editItem} />
+              <button className='clear-btn' onClick={clearList}>
+                clear items
+              </button>
+            </div>
+          )}
+        </section>
+      </Route>
+    </Routes>
   );
 }
 
